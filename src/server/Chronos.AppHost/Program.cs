7a7d9682799c4b96bb0aa9337f33271701d8738b
Josh Robertson <josh.robertson@enhancedigital.co.uk>
2024-06-24 22:25:50 +0100

    Added bus

--- src/server/Chronos.AppHost/Program.cs
+++ src/server/Chronos.AppHost/Program.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Chronos_Web>("chronos-api");
+builder.AddProject<Projects.Chronos_Bus>("chronos-bus");
 
 builder.Build().Run();
