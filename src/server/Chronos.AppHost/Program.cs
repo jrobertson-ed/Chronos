bb488f93edbafd48d9e3f06ede7c55ad88b97f43
Josh Robertson <josh.robertson@enhancedigital.co.uk>
2024-10-06 13:14:53 +0100

    Added client

--- src/server/Chronos.AppHost/Program.cs
+++ src/server/Chronos.AppHost/Program.cs
@@ -1,6 +1,20 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Chronos_Web>("chronos-api");
-builder.AddProject<Projects.Chronos_Bus>("chronos-bus");
+var db = builder.AddPostgres("chronos-db")
+    .WithDataVolume()
+    .AddDatabase("Chronos");
+
+var api = builder.AddProject<Projects.Chronos_Web>("chronos-api")
+    .WithExternalHttpEndpoints()
+    .WithReference(db);
+
+builder.AddProject<Projects.Chronos_Bus>("chronos-bus")
+    .WithReference(db);
+
+builder.AddNpmApp("chronos-spa", "../../client")
+    .WithReference(api)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
---
+++ src/server/Chronos.Core/Entities/TimesheetEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chronos.Core.Entities
+{
+    public class TimesheetEntry : TenantEntity<Guid>
+    {
+        public Guid ClientId { get; set; }
+        public Client? Client { get; set; }
+    }
+}
